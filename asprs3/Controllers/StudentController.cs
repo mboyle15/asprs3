01f57381e5ae7f98c6626152416ebe9864d20493
mboyle15 <mboyle15@alaska.edu>
2016-12-15 22:34:47 -0900

    aded student controller and select student page

--- asprs3/Controllers/StudentController.cs
+++ asprs3/Controllers/StudentController.cs
@@ -1,89 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using asprs3.Models;
+using System.Web.WebPages;
+using asprs3.Models;
+
 
 namespace asprs3.Controllers
 {
     public class StudentController : Controller
     {
-        // GET: Student
-        public ActionResult Index()
+        Model1 db = new Model1();
+        // GET: Students
+        public StudentController()
+        {
+            db = new Model1();
+        }
+
+
+        public ActionResult SelectStudent()
         {
             return View();
         }
 
-        // GET: Student/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Student/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Student/Create
+        /*
+        [Authorize(Roles = "Parent")]
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult AddStudent(Student viewModel)
+        //Get entered Student information and query it against Students, if student exists in Students, add Guardianship entity
         {
-            try
-            {
-                // TODO: Add insert logic here
+            List<int> StudentList = new List<int>();
+            var parent = User.Identity.GetUserId();
 
-                return RedirectToAction("Index");
-            }
-            catch
+            var F_Name = viewModel.Stud_F_Name;
+            var L_Name = viewModel.Stud_L_Name;
+            var DOB = viewModel.Date_Of_Birth;
+            bool added = false;
+
+
+            var existingStudent = from m in db.Students
+                                  where m.Stud_F_Name == F_Name && m.Stud_L_Name == L_Name
+                                  select m;
+
+            var dateCheck = from m in existingStudent
+                            where DOB.Equals(m.Date_Of_Birth)
+                            select m.Student_Number;
+
+            foreach (int s in dateCheck)
             {
-                return View();
+                Guardianship guardianship = new Guardianship();
+                guardianship.UserName = parent;
+                guardianship.Student_Number = s;
+                db.Guardianships.Add(guardianship);
+                added = true;
             }
+
+            db.SaveChanges();
+            if (!added)
+            {
+                return RedirectToAction("AddStudent", "Students");
+            }
+
+
+
+            return RedirectToAction("ManageStudent", "Students");
         }
-
-        // GET: Student/Edit/5
-        public ActionResult Edit(int id)
+        [Authorize(Roles = "Parent")]
+        public ActionResult ManageStudent()
         {
+            var userId = User.Identity.GetUserId();
+
+            // Queryable list of Guardianships with a matching username and to the user in session.
+            var studentId = from m in db.Guardianships
+                            where m.Parent.Id == userId
+                            select m.Student_Number;
+
+            var students = from s in db.Students
+                           where studentId.Contains(s.Student_Number)
+                           select s;
+
+            return View(students.ToList());
+        }
+        [Authorize(Roles = "Parent")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteStudent()
+        {
+            string userId = User.Identity.GetUserId();
+            int id = Request.Form["studentId"].AsInt();
+
+            ViewBag.Stud_F_Name = db.Students.Find(id).Stud_F_Name;
+            ViewBag.Stud_L_Name = db.Students.Find(id).Stud_L_Name;
+            ViewBag.Date_Of_Birth = db.Students.Find(id).Date_Of_Birth.ToShortDateString();
+
+            var guardianshipId = from m in db.Guardianships
+                                 where m.UserName == userId && m.Student_Number == id
+                                 select m;
+
+            if (guardianshipId.Count() > 1)
+            {
+                return RedirectToAction("DeleteStudent", "Students");
+            }
+            var output = guardianshipId.First();
+
+            return View(output);
+        }
+        [Authorize(Roles = "Parent")]
+        public ActionResult ConfirmDeleteStudent(int id)
+        {
+            //Deletes relation between parent & student
+            Guardianship delete = db.Guardianships.Find(id);
+            if (delete == null)
+            {
+                return RedirectToAction("ManageStudent", "Students");
+            }
+            db.Guardianships.Remove(delete);
+            db.SaveChanges();
+
+            return RedirectToAction("ManageStudent", "Students");
+        }
+        [Authorize(Roles = "Parent")]
+        public ActionResult AddGuardianship()
+        {
+
             return View();
         }
-
-        // POST: Student/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Student/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Student/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        */
     }
 }
